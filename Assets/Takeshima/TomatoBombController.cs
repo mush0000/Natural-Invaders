58f79165cf83ee438a3bf64c7e4d8e27c46e54f6
takeshimanZ <takeshimanz0909@gmail.com>
2023-12-19 12:04:51 +0900

    tomato bomb add

--- Assets/Takeshima/TomatoBombController.cs
+++ Assets/Takeshima/TomatoBombController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class TomatoBombController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
     void Start()
     {
-        
+        Application.targetFrameRate = 60;
+        Shoot(new Vector3(0, 200, 2000));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
