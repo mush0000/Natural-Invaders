69d2a751e3bd1bc519c7b80d825b9fdef38c3e88
ejm-knt <syokugyoukunren.ejima@gmail.com>
2023-12-23 12:55:41 +0900

    EnemyClass関連の編集 【実装内容】 ・EnemyClass内に動作確認用デバック記述の追加 ・Enemyに関与するテスト用PlayerClassの作成 ・EnemyとPlayerの相互動作を確認するためのテスト用GameDirectorの作成 ~~ 【備考】 ・EnemyAction抽象メソッド記述中 　→　数点コメントアウトでコンパイルエラーを回避している。修正必須。早期に解決されたし。

--- Assets/Ejima/DebugFolder/EnemyPlayerClassDebugTest/EnemyChild.cs
+++ Assets/Ejima/DebugFolder/EnemyPlayerClassDebugTest/EnemyChild.cs
@@ -41,54 +41,6 @@ public class EnemyChild : CharacterScript
         }
     }
 
-    // //! キャラクターのリストからターゲットを選択する関数
-    // public CharacterScript SelectTarget(List<CharacterScript> characters)
-    // {
-    //     //? 前列にいるキャラクターだけを含む新しいリストを作成
-    //     List<CharacterScript> frontRowCharacters = characters.Where(c => c.position <= 3).ToList();
-    //     if (frontRowCharacters.Count > 0)
-    //     {
-    //         //? 前列にキャラクターがいる場合、その中から一つを選択
-    //         return SelectCharacterFromRow(frontRowCharacters);
-    //     }
-
-    //     //? 中列にいるキャラクターだけを含む新しいリストを作成
-    //     List<CharacterScript> middleRowCharacters = characters.Where(c => c.position >= 4 && c.position <= 6).ToList();
-    //     if (middleRowCharacters.Count > 0)
-    //     {
-    //         //? 中列にキャラクターがいる場合、その中から一つを選択
-    //         return SelectCharacterFromRow(middleRowCharacters);
-    //     }
-
-    //     //? 後列にいるキャラクターだけを含む新しいリストを作成
-    //     List<CharacterScript> backRowCharacters = characters.Where(c => c.position >= 7 && c.position <= 9).ToList();
-    //     if (backRowCharacters.Count > 0)
-    //     {
-    //         //? 後列にキャラクターがいる場合、その中から一つを選択
-    //         return SelectCharacterFromRow(backRowCharacters);
-    //     }
-    //     return null; //* どの列にもキャラクターがいない場合、nullを返す
-    // }
-    // //! SelectTarget関数で生成された新リストの中身を元に乱数を確定し、ランダムなキャラクタークラスを返す関数。
-    // //! posi[4,6]の場合で乱数が5を生成した場合は、合致しないが、無限ループで乱数を再生成させるため、いつか必ず合致する。
-    // private CharacterScript SelectCharacterFromRow(List<CharacterScript> rowCharacters)
-    // {
-    //     int minPosition = rowCharacters.Min(c => c.position); //* 最小位置を取得
-    //     int maxPosition = rowCharacters.Max(c => c.position); //* 最大位置を取得
-    //     while (true)
-    //     {
-    //         int randPosition = UnityEngine.Random.Range(minPosition, maxPosition + 1); //* 最小位置と最大位置の間で乱数を生成
-    //         foreach (CharacterScript character in rowCharacters)
-    //         {
-    //             if (character.position == randPosition) //* 生成した乱数がキャラクターの位置と一致する場合
-    //             {
-    //                 return character; //* そのキャラクターを返す
-    //             }
-    //         }
-    //     }
-    // }
-
-
     //! キャラクターのリストからターゲットを選択する関数
     public List<CharacterScript> SelectTargetGroups(List<CharacterScript> characters)
     {
