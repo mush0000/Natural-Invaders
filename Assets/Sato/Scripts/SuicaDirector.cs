fbede685948358447aea5542df05514c1d566cc1
shizen-12 <katudon4649_2@hotmail.com>
2024-01-06 14:41:44 +0900

    1/6終了時

---
+++ Assets/Sato/Scripts/SuicaDirector.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuicaDirector : MonoBehaviour
+{
+    [SerializeField] GameObject banana;
+    [SerializeField] GameObject potato;
+    [SerializeField] GameObject pumpkin;
+    [SerializeField] GameObject tomato;
+    [SerializeField] GameObject peach;
+    [SerializeField] GameObject grape;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
