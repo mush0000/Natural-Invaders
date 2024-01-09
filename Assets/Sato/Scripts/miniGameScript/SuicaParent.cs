3f9c8059b84c3bd15ef8a151bf73b3e362c5f38a
shizen-12 <katudon4649_2@hotmail.com>
2024-01-09 12:37:52 +0900

    minigame作成中

---
+++ Assets/Sato/Scripts/miniGameScript/SuicaParent.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SuicaParent : MonoBehaviour
+{
+    public GameObject nextPrefab;
+    SuicaDirector suica;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject suicaDirector = GameObject.Find("SuicaDirector");
+        suica = suicaDirector.GetComponent<SuicaDirector>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == gameObject.tag)
+        {
+            if (this.gameObject.GetInstanceID() > other.gameObject.GetInstanceID())
+            {//インスタンスIDが大きいほうだけがこのメソッドを動かします。
+                // 当たったGameObjectを非表示にしてQueueに戻します。
+                suica.ReturnObject(this.gameObject);
+                suica.ReturnObject(other.gameObject);
+                //新しい場所に生成します
+                if (nextPrefab != null)
+                {   //nextPrefabが存在したら、最後の果物にはつけない
+                    suica.CreateUnionObject(nextPrefab, this.gameObject.transform.position, other.gameObject.transform.position);
+                }
+            }
+        }
+    }
+}
