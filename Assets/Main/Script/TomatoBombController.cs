2e79af61463df6832eed49d76de6796cc8813047
takeshimanZ <takeshimanz0909@gmail.com>
2024-01-10 14:52:34 +0900

    キャラ固有のScript追加

--- Assets/Main/Script/TomatoBombController.cs
+++ Assets/Main/Script/TomatoBombController.cs
@@ -1,109 +1,109 @@
-using UnityEngine;
+// using UnityEngine;
 
-public class TomatoBombController : MonoBehaviour
-{
-    public GameObject ExplosionEffect;
-    public GameObject ExplosionSpark;
-    private Vector3 originalPosition;
-    private Quaternion originalRotation; // 元の回転の値を保存
-    private bool hasExploded = false;
+// public class TomatoBombController : MonoBehaviour
+// {
+//     public GameObject ExplosionEffect;
+//     public GameObject ExplosionSpark;
+//     private Vector3 originalPosition;
+//     private Quaternion originalRotation; // 元の回転の値を保存
+//     private bool hasExploded = false;
 
-    public Vector3 OriginalPosition
-    {
-        get { return originalPosition; }
-    }
+//     public Vector3 OriginalPosition
+//     {
+//         get { return originalPosition; }
+//     }
 
-    public Vector3 TargetEnemy
-    {
-        get { return originalPosition; }
-    }
+//     public Vector3 TargetEnemy
+//     {
+//         get { return originalPosition; }
+//     }
 
-    void Start()
-    {
-        originalPosition = transform.position;
-        originalRotation = transform.rotation; // インスタンス化されたときに元の回転を保存する
-    }
+//     void Start()
+//     {
+//         originalPosition = transform.position;
+//         originalRotation = transform.rotation; // インスタンス化されたときに元の回転を保存する
+//     }
 
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Enemy"))
-        {
-            if (!hasExploded)
-            {
-                ContactPoint contact = collision.contacts[0];
-                Vector3 enemyCenter = contact.point;
-                Instantiate(ExplosionEffect, enemyCenter, Quaternion.identity);
-                Instantiate(ExplosionSpark, enemyCenter, Quaternion.identity);
+//     void OnCollisionEnter(Collision collision)
+//     {
+//         if (collision.gameObject.CompareTag("Enemy"))
+//         {
+//             if (!hasExploded)
+//             {
+//                 ContactPoint contact = collision.contacts[0];
+//                 Vector3 enemyCenter = contact.point;
+//                 Instantiate(ExplosionEffect, enemyCenter, Quaternion.identity);
+//                 Instantiate(ExplosionSpark, enemyCenter, Quaternion.identity);
 
-                if (!collision.gameObject.CompareTag("TomatoBomb"))
-                {
-                    GetComponent<Rigidbody>().isKinematic = true;
-                    transform.position = originalPosition;
-                    // Reset the rotation to the original state
-                    transform.rotation = originalRotation;
-                    // originalPositionを更新
-                    originalPosition = transform.position;
-                }
+//                 if (!collision.gameObject.CompareTag("TomatoBomb"))
+//                 {
+//                     GetComponent<Rigidbody>().isKinematic = true;
+//                     transform.position = originalPosition;
+//                     // Reset the rotation to the original state
+//                     transform.rotation = originalRotation;
+//                     // originalPositionを更新
+//                     originalPosition = transform.position;
+//                 }
 
-                hasExploded = true;
-            }
-        }
-    }
+//                 hasExploded = true;
+//             }
+//         }
+//     }
 
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-        {
-            // クリック時にhasExplodedをリセット
-            hasExploded = false;
+//     void Update()
+//     {
+//         if (Input.GetMouseButtonDown(0))
+//         {
+//             // クリック時にhasExplodedをリセット
+//             hasExploded = false;
 
-            // TomatoBombの位置からRayを飛ばす
-            Ray ray = new Ray(transform.position, transform.forward); // transform.forwardはTomatoBombの前方を表すベクトル
-            RaycastHit hit;
+//             // TomatoBombの位置からRayを飛ばす
+//             Ray ray = new Ray(transform.position, transform.forward); // transform.forwardはTomatoBombの前方を表すベクトル
+//             RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit))
-            {
-                // 目標地点を元の位置から引いて、方向ベクトルを得る
-                Vector3 shootDirection = hit.point - originalPosition;
+//             if (Physics.Raycast(ray, out hit))
+//             {
+//                 // 目標地点を元の位置から引いて、方向ベクトルを得る
+//                 Vector3 shootDirection = hit.point - originalPosition;
 
-                // Enemyの位置に応じて力を自動調整する
-                AdjustForce(hit.collider, shootDirection);
-            }
-        }
-    }
+//                 // Enemyの位置に応じて力を自動調整する
+//                 AdjustForce(hit.collider, shootDirection);
+//             }
+//         }
+//     }
 
-    void AdjustForce(Collider enemyCollider, Vector3 shootDirection)
-    {
-        // Enemyの位置に応じて力を計算する
-        float distanceToEnemy = Vector3.Distance(originalPosition, enemyCollider.bounds.center);
-        float upwardForce = CalculateUpwardForce(distanceToEnemy);
-        float rotationSpeed = CalculateRotationSpeed(distanceToEnemy);
+//     void AdjustForce(Collider enemyCollider, Vector3 shootDirection)
+//     {
+//         // Enemyの位置に応じて力を計算する
+//         float distanceToEnemy = Vector3.Distance(originalPosition, enemyCollider.bounds.center);
+//         float upwardForce = CalculateUpwardForce(distanceToEnemy);
+//         float rotationSpeed = CalculateRotationSpeed(distanceToEnemy);
 
-        // Shootメソッドを呼び出して物体を飛ばす
-        Shoot(shootDirection.normalized * 3000f, upwardForce, rotationSpeed);
-        // hasExploded = false;
-    }
+//         // Shootメソッドを呼び出して物体を飛ばす
+//         Shoot(shootDirection.normalized * 3000f, upwardForce, rotationSpeed);
+//         // hasExploded = false;
+//     }
 
-    public float CalculateUpwardForce(float distanceToEnemy)
-    {
-        // 距離が近いほど上向きの力を増やす
-        return Mathf.Max(300f, 900f - distanceToEnemy * 10f);
-    }
+//     public float CalculateUpwardForce(float distanceToEnemy)
+//     {
+//         // 距離が近いほど上向きの力を増やす
+//         return Mathf.Max(300f, 900f - distanceToEnemy * 10f);
+//     }
 
-    public float CalculateRotationSpeed(float distanceToEnemy)
-    {
-        // 距離が遠いほど回転速度を増やす
-        return Mathf.Min(300f, distanceToEnemy * 5f);
-    }
+//     public float CalculateRotationSpeed(float distanceToEnemy)
+//     {
+//         // 距離が遠いほど回転速度を増やす
+//         return Mathf.Min(300f, distanceToEnemy * 5f);
+//     }
 
-    public void Shoot(Vector3 dir, float upwardForce, float rotationSpeed)
-    {
-        Rigidbody rb = GetComponent<Rigidbody>();
-        rb.isKinematic = false;
+//     public void Shoot(Vector3 dir, float upwardForce, float rotationSpeed)
+//     {
+//         Rigidbody rb = GetComponent<Rigidbody>();
+//         rb.isKinematic = false;
 
-        rb.rotation = Quaternion.identity;
-        rb.AddTorque(new Vector3(1, 1, 1) * rotationSpeed, ForceMode.Impulse);
-        rb.AddForce(Vector3.up * upwardForce);
-        rb.AddForce(dir);
-    }
-}
+//         rb.rotation = Quaternion.identity;
+//         rb.AddTorque(new Vector3(1, 1, 1) * rotationSpeed, ForceMode.Impulse);
+//         rb.AddForce(Vector3.up * upwardForce);
+//         rb.AddForce(dir);
+//     }
+// }
---
+++ Assets/Takeshima/Script/Carrot.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carrot : CharacterScript
+{
+    // private GameObject Sword; // Swordオブジェクト
+    // private GameObject Shield; // Shieldオブジェクト
+    // private bool isSwordOn = false; // Swordオブジェクトのオン/オフの状態を管理
+    // private bool isShieldOn = false; // Shieldオブジェクトのオン/オフの状態を管理
+
+    public Carrot() : base("Carrot", 20, 10, 8, 20, 0, 0, 12)
+    {
+        // 親クラス(CharacterScript)のコンストラクタを呼び出す
+        // ここで追加の初期化などを行うこともできます
+    }
+
+    public override void FrontAction()
+    {
+        base.FrontAction();
+        // ToggleSword();
+        // 固有キャラの前列行動の処理を追加
+    }
+
+    private void FrontActionSound()
+    {
+        // サウンド再生のロジック
+        // SoundManager.instance.PlaySE(SoundManager.SE_Type.〇〇);
+        // SoundManager.instance.StopSE();
+    }
+    private void FrontActionEffect()
+    {
+        // エフェクトプレハブの生成と再生
+        // GameObject effectInstance = Instantiate(attackEffectPrefab, transform.position, Quaternion.identity);
+        // Destroy(effectInstance, particleSystem.main.duration);  // エフェクトが終了したら削除
+
+        // ここにアニメーションを再生するコードを追加
+        // animator.Play("YourAnimationName");
+        // SoundManager.instance.PlaySE(SoundManager.SE_Type.〇〇);
+        // SoundManager.instance.StopSE();
+    }
+
+    public override void MiddleAction()
+    {
+        base.MiddleAction();
+        // ToggleShield();
+        // Potato独自の中列行動の処理を追加
+    }
+
+    // void ToggleShield()
+    // {
+    //     // オンの場合はオフに、オフの場合はオンにする
+    //     // animator.Play("YourAnimationName");
+    //     Shield.SetActive(!isShieldOn);
+    //     // 状態を反転
+    //     isShieldOn = !isShieldOn;
+
+    // }
+    public override void BackAction()
+    {
+        base.BackAction();
+        // Potato独自の後列行動の処理を追加
+    }
+
+    public override void AutoHeal()
+    {
+        base.AutoHeal();
+    }
+
+
+    private void MiddleActionSound()
+    {
+        // サウンド再生のロジック
+        // SoundManager.instance.PlaySE(SoundManager.SE_Type.〇〇);
+        // SoundManager.instance.StopSE();
+    }
+    private void MiddleActionEffect()
+    {
+        // エフェクトプレハブの生成と再生
+        // GameObject effectInstance = Instantiate(attackEffectPrefab, transform.position, Quaternion.identity);
+        // Destroy(effectInstance, particleSystem.main.duration);  // エフェクトが終了したら削除
+    }
+
+    private void BackActionSound()
+    {
+        // サウンド再生のロジック
+        // SoundManager.instance.PlaySE(SoundManager.SE_Type.〇〇);
+        // SoundManager.instance.StopSE();
+    }
+    private void BackActionnEffect()
+    {
+        // エフェクトプレハブの生成と再生
+        // GameObject effectInstance = Instantiate(attackEffectPrefab, transform.position, Quaternion.identity);
+        // Destroy(effectInstance, particleSystem.main.duration);  // エフェクトが終了したら削除
+    }
+
+    public override void Death()
+    {
+        base.Death();
+        // キャラの生死判定
+    }
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // // Shieldオブジェクトを取得
+        // Shield = transform.Find("Shield").gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // マウスの左クリックがされたら
+        // if (Input.GetMouseButtonDown(0))
+        // {
+        //     // オンとオフを切り替えるメソッドを呼び出す
+        //     ToggleSword();
+        //     ToggleShield();
+    }
+}
